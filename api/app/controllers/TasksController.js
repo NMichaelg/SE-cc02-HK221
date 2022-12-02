e9e630d44620d7d74049e48509bfb4d59bcf85e6
Khoa A. Pham <khoa.a.pham@opswat.com>
2022-12-02 19:22:36 +0700

    Up sample file

--- api/app/controllers/TasksController.js
+++ api/app/controllers/TasksController.js
@@ -1,6 +1,34 @@
+// fs = require("fs");
+
+// var task_db_path = "./app/database/tasks_info.js"
+// var task_db = JSON.parse(fs.readFileSync(task_db_path));
+
 class TasksController{
     one_day(req, res, next){
-        
+        res.send(
+            {
+              "user_info":{
+                "name": "Khoa"
+              },
+              
+              "detail": [
+                {
+                  "date":  "20-11-2020",
+                  "assignee": ["Khoa", "An"]
+                },
+                {
+                  "date":  "20-11-2020",
+                  "assignee": ["Khoa", "An"]
+                },
+                {
+                  "date":  "20-11-2020",
+                  "assignee": ["Khoa", "An"]
+                },
+              ]
+            }
+          );
     }
 
 }
+
+module.exports = new TasksController();
