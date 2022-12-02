769045302e84fd289fe0a1c58d892ff4ba4ca30a
Khoa A. Pham <khoa.a.pham@opswat.com>
2022-12-02 20:18:08 +0700

    Refactoring backend

--- api/app/controllers/TasksController.js
+++ api/app/controllers/TasksController.js
@@ -1,35 +1,10 @@
 fs = require("fs");
 
-// var task_db_path = "./app/database/tasks_info.js"
-// var task_db = JSON.parse(fs.readFileSync(task_db_path));
+const db_dir = "./app/database/";
 
 class TasksController{
-    one_day(req, res, next){
-        res.send(
-            {
-              "user_info":{
-                "name": "Khoa"
-              },
-              
-              "detail": [
-                {
-                  "date":  "20-11-2020",
-                  "assignee": ["Khoa", "An"]
-                },
-                {
-                  "date":  "20-11-2020",
-                  "assignee": ["Khoa", "An"]
-                },
-                {
-                  "date":  "20-11-2020",
-                  "assignee": ["Khoa", "An"]
-                },
-              ]
-            }
-          );
-    }
     calendar_page(req,res,next){
-      var user_list = JSON.parse(fs.readFileSync('./app/database/user_info.json'));
+      var user_list = JSON.parse(fs.readFileSync(db_dir + 'user_info.json'));
       res.send(user_list);
     }
 }
--- api/server.js
+++ api/server.js
@@ -2,18 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const taskController = require("./app/controllers/TasksController");
+
 const app = express();
 
 const port = 6868;
 
-const TaskControl = require("./app/controllers/TasksController.js")
-// var corsOptions = {
-//   origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
-// };
-
-// app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
@@ -24,9 +18,14 @@ const db = require("./app/models");
 db.sequelize.sync();
 
 // simple route
-app.get("/calendar", TaskControl.calendar_page);
+// route(app);
+
+console.log(__dirname);
+
+app.get("/calender", taskController.one_day);
 
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
