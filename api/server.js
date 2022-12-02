86fe9f36384cc4174910da6b80cdefe6699dd078
Khoa A. Pham <khoa.a.pham@opswat.com>
2022-12-02 14:18:56 +0700

    backend: Fix backend not working

--- api/server.js
+++ api/server.js
@@ -4,13 +4,13 @@ const cors = require("cors");
 
 const app = express();
 
-console.log("Test change");
+const port = 6868;
 
-var corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
-};
+// var corsOptions = {
+//   origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -24,5 +24,12 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to UWC hehe 2.0 !!" });
+  // res.json({ "message": "Welcome to UWC hehe 2.0 !!" });
+  res.send("hehehe");
+
+});
+
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
