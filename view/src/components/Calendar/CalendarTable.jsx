e6539e6a66f1553e995703d8759d5783079ae6c9
Minh Phuoc <phuoc.nguyenlhp2508@hcmut.edu.vn>
2022-12-05 18:37:45 +0700

    MP -add breadcrumb, calendar

---
+++ view/src/components/Calendar/CalendarTable.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import "./calendarTable.scss"
+
+function handleDayClick(){
+  console.log()
+}
+
+function CalendarTable({ month }) {
+  return (
+    <div className='calendar-container'>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        }}
+      />
+    </div>
+  )
+}
+
+export default CalendarTable
